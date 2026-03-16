d3809a7a04b4b9c06f5e42df07d8e174db824a55
Sathija-Balachandra007 <harknesssharrid@gmail.com>
2026-03-16 13:59:08 +0530

    Index.js

---
+++ Index.js
@@ -0,0 +1,20 @@
+{
+  "name": "fuelpass-backend",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "bcryptjs": "^2.4.3",
+    "body-parser": "^1.20.2",
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2",
+    "jsonwebtoken": "^9.0.1",
+    "moment": "^2.30.0",
+    "pg": "^8.12.0",
+    "qrcode": "^1.5.1",
+    "uuid": "^9.0.1"
+  }
+}
