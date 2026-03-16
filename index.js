e42426d93e5e0f2aac2c043ddd68a4a623078656
Sathija-Balachandra007 <harknesssharrid@gmail.com>
2026-03-16 14:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,21 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Mount routes
 app.use('/auth', authRoutes);
 app.use('/vehicle', vehicleRoutes);
 app.use('/fuelpass', fuelpassRoutes);
 app.use('/transaction', transactionRoutes);
 
+// Default route to check server is running
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Catch-all for unknown routes
+app.use((req, res) => {
+  res.status(404).send('Route not found');
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
